561e4c9d3b30ba543a4073db64fcd5dc9ed39a7f
MagicCat00 <75489180+MagicCat00@users.noreply.github.com>
2022-03-01 15:54:23 +0300

    Add files via upload

---
+++ task.js
@@ -0,0 +1,39 @@
+class Transport {
+  constructor(type, price, vendor, model) {
+    Transport.type = type;
+    Transport.price = price;
+    Transport.vendor = vendor;
+    Transport.model = model;
+  }
+
+  getInfo() {
+    return `${Transport.vendor}, ${Transport.model}`;
+  }
+
+  getPrice() {
+    return Transport.price.toLocaleString('ru-RU') + ' ₽';
+  }
+}
+
+class Car extends Transport {
+  constructor(vendor, model, doorsCount, price) {
+    super('car', price, vendor, model);
+    Transport.doorsCount = doorsCount;
+  }
+
+  getDoorsCount() {
+    return `Кол-во дверей: ${Transport.doorsCount}`;
+  }
+}
+
+class Bike extends Transport {
+  constructor(vendor, model, maxSpeed, price) {
+    super('bike', price, vendor, model);
+    Transport.maxSpeed = maxSpeed;
+  }
+
+  
+  getMaxSpeed() {
+    return `Макс. скорость: ${Transport.maxSpeed} км/ч`;
+  }
+}
